90b73015d01c85623bcfa3584e1e746a76828ef2
MuhammadSahimBhaur <21100317@lums.edu.pk>
2023-04-18 22:00:36 +0500

    Investigating nested routes

--- src/Random2.js
+++ src/Random2.js
@@ -5,6 +5,8 @@ export const Random2 = () => {
     <div>Random2
       <div><Link to="/">Back to Home!!</Link></div>
       <Link to="Child">To Child!</Link>
+      <Link to="Child2">To Child2!</Link>
+      <Outlet />
       <Outlet />
     </div>
 
